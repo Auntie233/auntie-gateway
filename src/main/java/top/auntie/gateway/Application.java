54eb4e81029048c851fc9cf3b339fea688ce8d54
Auntie233 <tai1992@qq.com>
2020-10-13 09:09:17 +0800

    init

---
+++ src/main/java/top/auntie/gateway/Application.java
@@ -0,0 +1,14 @@
+package top.auntie.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+
+@SpringCloudApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/top/auntie/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,9 @@
+package top.auntie.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+
+}
